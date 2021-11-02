acef886730d13a0a4777459ca07d10af0d0080b8
LightBlueGamer <timjonsson03@hotmail.com>
2021-11-02 17:36:01 +0100

    Added required files

---
+++ src/main/java/net/raylightstudios/carved/Carved.java
@@ -0,0 +1,24 @@
+package net.raylightstudios.carved;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Carved.MOD_ID)
+public class Carved {
+    public static final String MOD_ID = "carved";
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public Carved() {
+        // Register the setup method for modloading
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+}
